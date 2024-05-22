ceed96c8b39862c023c27ba16c89cf21f9c006a3
shmuli <shmulikeller@gmail.com>
2024-05-22 01:31:15 -0400

    updated_icon_path

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.static_folder = 'static'
 
 
 email_address = "shmulikeller@gmail.com"
 
-@app.route('/page')
+@app.route('/')
 def index():
     return render_template('main.html', email_address=email_address)
 
